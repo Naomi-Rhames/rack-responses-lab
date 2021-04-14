11868152092f4a48678bccb0c03cb1ff66cadf3c
Naomi-Rhames <naomirhames2020@gmail.com>
2021-04-14 09:22:12 -0500

    commit tree 8bc7f3619c2815586121138da08f8850040ad6f5

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,12 @@ class Application
     def call(env)
         resp = Rack::Response.new
         time = Time.now.hour
+         binding.pry
             if time <= 12
                 resp.write "Good Morning"
             else
                 resp.write "Good Afternoon"
+            
             end
         resp.finish
     end
